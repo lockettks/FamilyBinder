1ff55802da862100b56f51e6bcf21f6d829840ad
Kim Mathieu <lockettks@gmail.com>
2018-04-29 00:12:23 -0400

    Fix placement of menu

--- FamilyBinder/CircleMenuView.swift
+++ FamilyBinder/CircleMenuView.swift
@@ -10,31 +10,22 @@ import UIKit
 
 class CircleMenuView: UIView {
     
-    var circleDiameter: CGFloat
+    var circleRadius: CGFloat
+    let frameWidth = CGFloat(300)
+    let frameHeight = CGFloat(300)
     let menuRadius = Float(75.0)
     var fillColors: [CGColor]
     var images: [UIImage]?
     var circleButtons = [CircleButton]()
     let circleMenuService = CircleMenuService()
-    let menuWidth = CGFloat(200)
-    let menuHeight = CGFloat(200)
+
     
-    
-    
-    init(touchPoint: CGPoint, fillColors: [CGColor], circleImages: [UIImage]?, circleDiameter: CGFloat = 50.0) {
-        let menuFrame = CGRect(x: touchPoint.x-self.menuWidth/2, y: touchPoint.y-self.menuHeight/2, width: self.menuWidth, height: self.menuHeight)
-//        let origin = CGPoint(x: 0, y: 0)
-        let menuCenter = CGPoint(x: CGFloat(menuRadius), y: CGFloat(menuRadius))
-        self.circleDiameter = circleDiameter
-//        if let circleDiameter = circleDiameter {
-//            self.circleDiameter = circleDiameter
-//        } else {
-//            self.circleDiameter = 50.0
-//        }
-        
-        //let menuFrame = CGRect(x: origin.x, y: origin.y, width: 200, height: 200)
-        
-        
+    init(touchPoint: CGPoint, fillColors: [CGColor], circleImages: [UIImage]?, circleRadius: CGFloat = 25.0) {
+        let menuFrame = CGRect(x: touchPoint.x-self.frameWidth/2, y: touchPoint.y-self.frameHeight/2, width: self.frameWidth, height: self.frameHeight)
+        let menuCenter = CGPoint(x: frameWidth/2, y: frameHeight/2)
+        print("menu center \(menuCenter)")
+        self.circleRadius = circleRadius
+
         self.fillColors = fillColors
         if let images = circleImages {
             if images.count == fillColors.count {
@@ -44,12 +35,11 @@ class CircleMenuView: UIView {
             }
         }
         
-        //super.init(frame: menuFrame)
         super.init(frame: menuFrame)
         
         for (index, fillColor) in self.fillColors.enumerated() {
             var circleButton : CircleButton
-            let circleFrame = CGRect(x: 0, y: 0, width: self.circleDiameter, height: self.circleDiameter)
+            let circleFrame = CGRect(x: 0, y: 0, width: self.circleRadius * 2, height: self.circleRadius * 2)
             if let circleImages = self.images {
                 circleButton = CircleButton(frame: circleFrame, fillColor: fillColor, circleImage: circleImages[index])
             } else {
